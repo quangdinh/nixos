406fedcb0f9ff4f6c2685324323848a0693cc633
Quang Dinh Le <lequangdinh@gmail.com>
2022-12-15 09:53:36 +0100

    Initial Configs

---
+++ configuration.nix
@@ -0,0 +1,184 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  networking.hostName = "Carbon"; # Define your hostname.
+
+  # ZSH
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+  environment.shells = with pkgs; [ zsh ];
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "nl_NL.UTF-8";
+    LC_IDENTIFICATION = "nl_NL.UTF-8";
+    LC_MEASUREMENT = "nl_NL.UTF-8";
+    LC_MONETARY = "nl_NL.UTF-8";
+    LC_NAME = "nl_NL.UTF-8";
+    LC_NUMERIC = "nl_NL.UTF-8";
+    LC_PAPER = "nl_NL.UTF-8";
+    LC_TELEPHONE = "nl_NL.UTF-8";
+    LC_TIME = "nl_NL.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.excludePackages = [ pkgs.xterm ];
+
+  # Disable core gnome apps
+  services.gnome.core-utilities.enable = false;
+
+  # Disable tour app
+  environment.gnome.excludePackages = [ pkgs.gnome-tour ];
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Bluetooth
+  services.blueman.enable = true;
+
+  hardware = {
+    bluetooth = {
+      enable = true;
+      settings.General = {
+        Enable = "Source,Sink,Media,Socket";
+      };
+    };
+  };
+
+  # Disable documentation
+  documentation.nixos.enable = false;
+
+  # Docker
+  virtualisation.docker.enable = true;
+  virtualisation.docker.enableOnBoot = false;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    media-session.config.bluez-monitor.rules = [
+      {
+        # Matches all cards
+        matches = [ { "device.name" = "~bluez_card.*"; } ];
+        actions = {
+          "update-props" = {
+            "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+            # mSBC is not expected to work on all headset + adapter combinations.
+            "bluez5.msbc-support" = true;
+            # SBC-XQ is not expected to work on all headset + adapter combinations.
+            "bluez5.sbc-xq-support" = true;
+          };
+        };
+      }
+      {
+        matches = [
+          # Matches all sources
+          { "node.name" = "~bluez_input.*"; }
+          # Matches all outputs
+          { "node.name" = "~bluez_output.*"; }
+        ];
+        actions = {
+          "node.pause-on-idle" = false;
+        };
+      }
+    ];
+
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.qdtc = {
+    uid = 1000;
+    isNormalUser = true;
+    description = "Quang Dinh Le";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    home = "/home/qdtc";
+  };
+
+  # Alias for nvim
+  environment.shellAliases = {
+    vim = "nvim";
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = import ./system-packages.nix pkgs;
+
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+
+}
---
+++ system-packages.nix
@@ -0,0 +1,66 @@
+pkgs: with pkgs; [
+  # System utilities
+  yadm
+  neovim
+  bottom
+  bat
+  procs
+  exa
+
+  # Yubikey
+  opensc
+  ccid
+
+  # Wayland
+  wl-clipboard
+  noto-fonts
+  noto-fonts-extra
+  noto-fonts-emoji
+
+  # Gnome
+  firefox
+  xdg-desktop-portal-gnome
+  kitty
+  gnome.geary
+  gnome.eog
+  gnome.gnome-calendar
+  evince
+  gnome.file-roller
+  gnome.gnome-screenshot
+  gnome.nautilus
+  gnome.sushi
+  gnome.gnome-calculator
+  gnome.gvfs
+  gnome.gnome-tweaks
+  gnome.totem
+  rhythmbox
+
+  # Development
+  vscode
+  git
+  gitui
+  delta
+  rustup
+  go
+  nodejs-18_x
+  yarn
+  nodePackages.npm
+  obsidian
+  #postman
+
+  # Docker
+  docker-compose
+
+  # Gimp
+  gimp
+
+  # 1Password
+  _1password-gui
+
+  # Gnome Shell
+  gnomeExtensions.pop-shell
+
+  # Theming
+  kora-icon-theme
+
+]
