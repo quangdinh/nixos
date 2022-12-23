4949238cc5576d5fdc5f8110cbd507130ac0bbce
Quang Dinh Le <lequangdinh@gmail.com>
2022-12-23 10:56:06 +0100

    Group

--- modules/qdtc.nix
+++ modules/qdtc.nix
@@ -4,7 +4,7 @@
     uid = 1000;
     isNormalUser = true;
     description = "Quang Dinh Le";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    extraGroups = [ "audio" "networkmanager" "wheel" "docker" ];
     home = "/home/qdtc";
     hashedPassword = "$6$43helWNzfEuDYcJ5$Sa2RTU0A1itX.MQWjj5PwSM.rMtrq6juENhiSKAgJl3kyUYxVWRLN9WkTJtFENJh8I9l26RchKk8H7PRwvLjG/";
   };
